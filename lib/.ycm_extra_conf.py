97dfd6f29e94a3f13902df869850ba9e923a8495
seu <seu@panopticon.re>
2015-04-21 22:42:39 +0200

    fix YCM config

--- lib/.ycm_extra_conf.py
+++ lib/.ycm_extra_conf.py
@@ -2,4 +2,4 @@ import os;
 
 def FlagsForFile(a):
 	dname = os.path.dirname(os.path.abspath(__file__ ))
-	return {'flags': ['-Wall','-std=c++11','-x','c++','-I/usr/include/libarchive','-I/usr/include/qt5/','-I',dname + "/../lib/include",'-I',dname + "/include",'-I',dname + "/../3rdparty/"], 'do_cache': True}
+	return {'flags': ['-Wall','-std=c++11','-x','c++','-I/usr/include/libarchive','-I/usr/include/qt5/QtQml','-I/usr/include/qt5/QtQuick','-I/usr/include/qt5/QtGui','-I/usr/include/qt5/QtWidgets','-I/usr/include/qt5/QtCore','-I',dname + "/../lib/include",'-I',dname + "/include",'-I',dname + "/../3rdparty/"], 'do_cache': True}
